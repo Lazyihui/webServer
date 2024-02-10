8626de2d91b55a4e2f6c9499602a4b20f0c25429
包子 <chengbaozi6@qq.com>
2024-02-10 23:43:27 +0800

    下载操作

--- Program.cs
+++ Program.cs
@@ -1,21 +1,28 @@
 using System.Text;
 
-public static class Program
-{
-    public static void Main(string[] args)
-    {
+
+public static class Program {
+    public static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
         //"/"相当于网址
-        app.MapGet("/", () =>
-        {
+        app.MapGet("/", () => {
             //return html
             // return Results.Text("<h1>hello World <h1>", "text/html");
             // ==
+            // 文件是字节流
             byte[] data = Encoding.UTF8.GetBytes("<h1>hello World <h1>");
             return Results.Text(data, "text/html");
+
         });
-        
+        //客服端请求文件
+        // download
+        //http://localhost:5218/dl 下载文件
+        app.MapGet("/dl", () => {
+            byte[] data = File.ReadAllBytes("c.zip");
+            return Results.File(data, "application/zip", "c.zip");
+        });
+
         app.MapGet("/hello", () => "yo");
 
 
